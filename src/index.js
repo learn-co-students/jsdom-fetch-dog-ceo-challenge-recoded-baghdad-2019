a2e1dfe7538e611d0b4edc773e1ad88913a01e05
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-09-18 12:45:05 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 console.log('%c HI', 'color: firebrick')
+
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+fetch('https://dog.ceo/api/breeds/image/random/4')
+.then(function(response) {
+  return response.json();
+}).then(function(json) {
+  let list;
+  for (let i = 0; i < 4; i++) {
+  list = document.createElement("li"); 
+  list.innerHTML = `<img src='${json["message"][i]}'>`;
+  document.getElementById("dog-breeds").appendChild(list); 
+}
+  
+    
+  
+});
+ 
+  
+  
+  
+
+  });
