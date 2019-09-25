6565ad53e4f29525818fb1560813e93b716127d9
Mohammed Saeb <mohammed.saeb98@gmail.com>
2019-09-25 17:41:31 +0000

    Challenge-1-complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+window.addEventListener("load", function(){
+  let imgs
+  let imgsContainer = document.getElementById("dog-image-container");
+  fetch(imgUrl).then(function(response) {
+    return response.json()
+  }).then( function (data) {
+    imgs = data.message
+    imgs.forEach(function(img){
+      let imgElement = document.createElement("img")
+      imgElement.src = img
+      imgsContainer.appendChild(imgElement)
+    })
+  })
+})
